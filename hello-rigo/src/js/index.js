46d88bf57146fc68667da3f6f7f53a7388fcc04b
Ariel Calisaya <calisayaariel62@gmail.com>
2019-07-30 23:25:09 +0000

    simpleCounterFinishEnd

--- hello-rigo/src/js/index.js
+++ hello-rigo/src/js/index.js
@@ -11,6 +11,7 @@ import "../styles/index.scss";
 
 function SimpleCounter(props) {
 	return (
+		/** Here is the limit of the counter, in this case I set the limit for 60 seconds per cell. */
 		<div className="here">
 			<h2 className="four">{props.digitFour % 60}</h2>
 			<h2 className="three">{props.digitThree % 60}</h2>
@@ -19,20 +20,23 @@ function SimpleCounter(props) {
 		</div>
 	);
 }
+/** In this lane we set the property of the simple counter function. */
 SimpleCounter.propTypes = {
 	digitFour: PropTypes.number,
 	digitThree: PropTypes.number,
 	digitTwo: PropTypes.number,
 	digitOne: PropTypes.number
 };
+/** This is the function that gives life to the stopwatch  */
 let counter = 0;
 setInterval(function() {
 	const four = Math.floor(counter / 1000);
 	const three = Math.floor(counter / 100);
-	const two = Math.floor(counter / 60);
+	const two = Math.floor(counter / 10);
 	const one = Math.floor(counter / 1);
 
 	//render your react application
+    /**counter ++ = adding the number per all digits */
 	counter++;
 	ReactDOM.render(
 		<SimpleCounter
@@ -41,6 +45,7 @@ setInterval(function() {
 			digitThree={three}
 			digitFour={four}
 		/>,
+        /**we print the function in this id */
 		document.querySelector("#app")
 	);
 }, 1000);
